55086c6999f6d800dd5bf5d02bbfdb25a54bd1df
LarisaChAng <LChS81@mail.ru>
2022-11-06 20:36:19 +0800

    Добавьте файлы проекта.

---
+++ Str_task/LinUravn.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Str_task
+{
+    struct LinUravn
+    {//Решение линейного уравнения типа: k*x+b=0
+        double k;
+        double b;
+
+        public LinUravn(double k, double b)
+        {
+            Console.WriteLine("Линейное уравнение вида: k*x+b=0");
+            this.k = k;
+            this.b = b;
+        }
+
+        public string Root()
+        {
+            if (k==0)
+                return "Решений нет - деление на ноль!";
+            double x = -b / k;
+            return $"Решение: x={x:0.00}";
+        }
+    }
+}
---
+++ Str_task/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Str_task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите коэффициенты k и b:");
+            double k = Convert.ToDouble(Console.ReadLine());
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            LinUravn linUravn = new LinUravn(k,b);
+            Console.WriteLine(linUravn.Root());
+            Console.ReadKey();
+        }
+    }
+}
